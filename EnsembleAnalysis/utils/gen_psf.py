4e1a73f738b5d1c89eb503a4070cadbc5b2c044b
jianhuang <jianhuang@umass.edu>
2025-07-15 14:32:03 -0400

    update

--- EnsembleAnalysis/utils/gen_psf.py
+++ EnsembleAnalysis/utils/gen_psf.py
@@ -17,7 +17,14 @@ def generate_psf_from_pdb(pdb, outpsf, N_ter_patch='NTER', C_ter_patch='CTER'):
     segid='A'
     gen.add_segment(segid=segid, pdbfile=pdb)
 
-    gen.patch(N_ter_patch, [(segid, gen.get_resids(segid)[0]),])
+    # specifial patches for PRO and GLY
+    if gen.get_resname('A', 0) == 'PRO':
+        gen.patch('PROP', [(segid, gen.get_resids(segid)[0]),])
+    elif gen.get_resname('A', 0) == 'GLY':
+        gen.patch('GLYP', [(segid, gen.get_resids(segid)[0]),])
+    else:
+        gen.patch(N_ter_patch, [(segid, gen.get_resids(segid)[0]),])
     gen.patch(C_ter_patch, [(segid, gen.get_resids(segid)[-1]),])
 
     gen.write_psf(outpsf)
+
