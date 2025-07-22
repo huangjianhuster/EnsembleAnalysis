9838b31e9593708315b66b68f9a1f5b89a2bc372
jianhuang <jianhuang@umass.edu>
2025-07-22 11:26:13 -0400

    update

--- EnsembleAnalysis/core/ensemble.py
+++ EnsembleAnalysis/core/ensemble.py
@@ -18,7 +18,7 @@ from multiprocessing import Pool
 from functools import partial
 import MDAnalysis as mda
 from MDAnalysis.analysis.dihedrals import Dihedral, Ramachandran
-from MDAnalysis.analysis import distances, contacts, rms, pcas
+from MDAnalysis.analysis import distances, contacts, rms, pca
 import mdtraj as md
 from Bio.PDB import PDBParser, DSSP
 mda.warnings.filterwarnings('ignore')
